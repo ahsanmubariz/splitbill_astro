bdb6a3775e15608fec542910dee935ab361af7a2
Ahsan Mubariz <ahsan.mubariz@digitalperuri.id>
2025-07-14 14:03:11 +0700

    feat: env adjust

--- src/pages/api/process-receipt.ts
+++ src/pages/api/process-receipt.ts
@@ -20,7 +20,7 @@ function arrayBufferToBase64(buffer: ArrayBuffer) {
 }
 
 export const POST: APIRoute = async (context) => {
-    const { request } = context;
+    const { request, locals } = context;
   // Check for the correct Content-Type header before processing
   const contentType = request.headers.get('content-type');
   if (!contentType || !contentType.includes('multipart/form-data')) {
@@ -33,7 +33,9 @@ export const POST: APIRoute = async (context) => {
     );
   }
   
-  const openRouterApiKey = import.meta.env.PRIVATE_OPENROUTER_API_KEY;
+  const openRouterApiKey =  locals.runtime?.env?.PRIVATE_OPENROUTER_API_KEY;
+
+
 
   if (!openRouterApiKey) {
     console.error("Server Error: OPENROUTER_API_KEY is not set in .env file.");
